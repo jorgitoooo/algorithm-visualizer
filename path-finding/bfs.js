97dee672880ed205baaa32479891c7b998b23fcd
Jorge Garcia <jlg.sftdev@gmail.com>
2019-09-05 13:04:06 -0700

    Fixed a bug where bfsAnimate() looped infinitely if destination node is unreachable

--- path-finding/bfs.js
+++ path-finding/bfs.js
@@ -1,22 +1,21 @@
-// Global variables
+// import Node from '../graph/node';
+/****************** Global variables ******************/
+// Speed of graph traversal
 const progressLimit = 30;
+
+// Node properties
 let nodes = [];
 const numOfNodesPerRow = 20;
 const size = numOfNodesPerRow - 1;
-// const wallColor = '#3c3c3c';
+
+// Colors
 const wallColor = '#fff';
 const cnvColor = wallColor;
 const srcColor = '#ff5a5f';
-// const visitedColor = '#087e8b';
-// const visitedColor = '#8469ad';
 const visitedColor = '#0baabc';
-// const destColor = '#44991b';
 const destColor = srcColor;
-// const destFoundColor = '#fcaa67';
 const destFoundColor = '#ff262d';
-
-/***************************************************/
-// import Node from '../graph/node';
+/******************************************************/
 class Node {
   constructor(x, y, width, height, color, isWall) {
     this.x = x;
@@ -32,7 +31,7 @@ class Node {
     return [this.x, this.y, this.width, this.height];
   }
 }
-
+/******************************************************/
 function getAdjacentVertices(x, y) {
   let adjList = [];
 
@@ -103,13 +102,12 @@ function unvisitedVerticesIn(adjList) {
       !nodes[coords[1]][coords[0]].isWall
   );
 }
-/******************************************************************/
+/******************************************************/
 (function initCanvas() {
   const canvas = document.getElementById('cnv');
   canvas.style.backgroundColor = cnvColor;
   canvas.width = window.innerWidth * 0.94;
   canvas.height = window.innerWidth * 0.94;
-  // canvas.style.padding = '2.5px';
   canvas.style.padding = `${canvas.width * 0.00625}px`;
 
   if (canvas.getContext) {
@@ -127,7 +125,7 @@ function unvisitedVerticesIn(adjList) {
     // Initialize graph
     for (let i = 0; i < numOfNodesPerRow; i++) {
       nodes[i] = [];
-      // let color = 'rgb(255,255,255)';
+
       let color = 'rgb(0,0,0)';
       for (let j = 0; j < numOfNodesPerRow; j++) {
         const isWall = Math.floor(Math.random() * 2);
@@ -144,6 +142,7 @@ function unvisitedVerticesIn(adjList) {
     alert('Canvas visualization unsupported by your browser =[');
     return;
   }
+  colorGrid();
 })();
 
 function colorGrid() {
@@ -153,46 +152,31 @@ function colorGrid() {
   nodes.forEach(nodesRow =>
     nodesRow.forEach(node => {
       ctx.fillStyle = node.color;
+
+      // Displays nodes as circles
+      // ctx.strokeStyle = node.color;
+      // ctx.beginPath();
+      // ctx.arc(
+      //   node.x + node.width / 2,
+      //   node.y + node.width / 2,
+      //   node.width / 2,
+      //   0,
+      //   2 * Math.PI
+      // );
+      // ctx.stroke();
+      // ctx.fill();
+
+      // Displays nodes as squares
       ctx.fillRect(...node.GetCoordsAndDim());
     })
   );
 }
-// let count = 0;
-// function bfs(source, destination) {
-//   let { x, y } = source;
 
-//   nodes[y][x].wasVisited = true;
-//   nodes[y][x].color = '#00b300';
-
-//   nodes[destination.y][destination.x].color = '#4ff4a2';
-
-//   let adjList = getAdjacentVertices(x, y);
-//   // let tmpList = [...adjList];
-
-//   // Loop through all adjacent vertices
-//   // while (adjList.length > 0) {
-//   while (count++ < 300 && adjList.length > 0) {
-//     // if (count > 100) break;
-//     setTimeout(() => {
-//       // tmpList.shift();
-
-//       let coords = adjList.shift();
-//       let [adjX, adjY] = coords;
-
-//       if (!nodes[adjY][adjX].isWall) {
-//         let tmpAdjList = getAdjacentVertices(adjX, adjY);
-//         // adjList.concat(tmpAdjList);
-//         adjList = [...adjList, ...tmpAdjList];
-//         // tmpList = [...tmpList, ...tmpAdjList];
-//         // console.log(tmpList);
-//         nodes[adjY][adjX].color = '#587';
-//         nodes[adjY][adjX].wasVisited = true;
-//       }
-
-//       colorGrid();
-//     }, 1000);
-//   }
-// }
+function clearGrid() {
+  const canvas = document.getElementById('cnv');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 /********************************************************/
 let source = {
@@ -236,6 +220,8 @@ function bfsAnimate(timestamp) {
         let tmpAdjList = getAdjacentVertices(adjX, adjY);
 
         adjList = [...adjList, ...tmpAdjList];
+
+        // Removes any duplicates that adjList may have
         removeDuplicates();
 
         if (adjX === destination.x && adjY === destination.y)
@@ -247,43 +233,40 @@ function bfsAnimate(timestamp) {
       colorGrid();
     }
   }
+
   stopId = requestAnimationFrame(bfsAnimate);
-  if (adjX == destination.x && adjY == destination.y) {
-    console.log(stopId);
+
+  if (adjList.length < 1 || (adjX == destination.x && adjY == destination.y)) {
     cancelAnimationFrame(stopId);
   }
 }
-stopId = requestAnimationFrame(bfsAnimate);
 
-function clearGrid() {
-  const canvas = document.getElementById('cnv');
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+setTimeout(() => (stopId = requestAnimationFrame(bfsAnimate)), 500);
 
+// Removes duplicates from coordinates array
 function removeDuplicates() {
-  // Split array of coordinates into x & y ordinate arrays
+  // Split array of coordinates into x- & y- coordinate arrays
   let xCoords = adjList.map(coord => coord[0]);
   let yCoords = adjList.map(coord => coord[1]);
 
-  // Used to map ordinates value to index
-  let xDup = {};
+  // Used to map ordinate's value to indices
+  let xMap = {};
 
   xCoords.forEach((x, idx) => {
-    if (xDup[x]) xDup[x] = [...xDup[x], idx];
-    else xDup[x] = [idx];
+    if (xMap[x]) xMap[x] = [...xMap[x], idx];
+    else xMap[x] = [idx];
   });
 
   // Used to tag potential duplicates
   let pDups = {};
 
-  // Used to save duplicate indices
+  // Used to save duplicates' indices
   let dupIndices = [];
 
   // Retrieve duplicates
-  for (let x in xDup) {
-    if (xDup[x].length > 1) {
-      xDup[x].forEach(idx => {
+  for (let x in xMap) {
+    if (xMap[x].length > 1) {
+      xMap[x].forEach(idx => {
         if (yCoords[idx] in pDups) {
           dupIndices = [...dupIndices, idx];
         } else {
@@ -298,5 +281,7 @@ function removeDuplicates() {
   // | in decreasing order of indices. This lets us modify array
   // | w/o modifying the indices of previous coordinates
   dupIndices.sort((el1, el2) => el1 < el2);
+
+  // Remove duplicates
   dupIndices.forEach(idx => adjList.splice(idx, 1));
 }
